1dd73525d97463b704a0a95defb0f95a96795dab
João Ceia <joao.p.ceia@gmail.com>
2021-12-14 12:37:42 +0000

    moved utils functions to utils.cpp

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,40 +6,15 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 02:48:53 by jceia             #+#    #+#             */
-/*   Updated: 2021/12/14 12:15:58 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/14 12:37:17 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
 #include <iostream>
-#include <limits>
 
-// case insensitive string comparison
-bool iequals(const std::string &s1, const std::string &s2)
-{
-    if (s1.size() != s2.size())
-        return false;
-    for (size_t i = 0; i < s1.size(); ++i)
-        if (tolower(s1[i]) != tolower(s2[i]))
-            return false;
-    return true;
-}
-
-// asks for an integer to the user
-// if the user enters an invalid integer, it returns false
-bool    getint(int &n)
-{
-    std::cin >> n;
-    std::cin.ignore();
-    if (std::cin.fail())
-    {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "The provided input is not an integer." << std::endl;
-        return false;
-    }
-    return true;
-}
+bool iequals(const std::string &s1, const std::string &s2);
+bool getint(int &n);
 
 int main(void)
 {
---
+++ ex01/src/utils.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 12:36:34 by jpceia            #+#    #+#             */
+/*   Updated: 2021/12/14 12:37:24 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <limits>
+
+// case insensitive string comparison
+bool iequals(const std::string &s1, const std::string &s2)
+{
+    if (s1.size() != s2.size())
+        return false;
+    for (size_t i = 0; i < s1.size(); ++i)
+        if (tolower(s1[i]) != tolower(s2[i]))
+            return false;
+    return true;
+}
+
+// asks for an integer to the user
+// if the user enters an invalid integer, it returns false
+bool    getint(int &n)
+{
+    std::cin >> n;
+    std::cin.ignore();
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "The provided input is not an integer." << std::endl;
+        return false;
+    }
+    return true;
+}
