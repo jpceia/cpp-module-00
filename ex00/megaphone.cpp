a208e2bf4505c646f40ca43510d54e42e9b37244
João Ceia <joao.p.ceia@gmail.com>
2021-12-14 13:39:39 +0000

    using strings in strupper

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,21 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 01:26:58 by jceia             #+#    #+#             */
-/*   Updated: 2021/11/01 03:03:40 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/14 13:39:24 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-char    *cstr_toupper(char *s)
+void    strupper(std::string& s)
 {
-    for (int i = 0; s[i] != '\0'; i++)
-        s[i] = toupper(s[i]);
-    return s;
+    for (unsigned int i = 0; i < s.length(); i++)
+        s[i] = std::toupper(s[i]);
 }
 
 int main(int argc, char *argv[])
@@ -27,7 +26,11 @@ int main(int argc, char *argv[])
     else
     {
         for (int i = 1; i < argc; i++)
-            std::cout << cstr_toupper(argv[i]);
+        {
+            std::string s = argv[i];
+            strupper(s);
+            std::cout << s;
+        }
     }
     std::cout << std::endl;
     return 0;
