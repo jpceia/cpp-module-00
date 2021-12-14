82cfe628cbcd2644d0e7929b8acfc707c528111b
João Ceia <joao.p.ceia@gmail.com>
2021-12-14 11:36:19 +0000

    added the iequals function for case insensitive string comparison

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -3,17 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 02:48:53 by jceia             #+#    #+#             */
-/*   Updated: 2021/11/01 05:07:57 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/14 11:12:45 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
 #include <iostream>
 
-int main()
+// case insensitive string comparison
+bool iequals(const std::string &s1, const std::string &s2)
+{
+    if (s1.size() != s2.size())
+        return false;
+    for (size_t i = 0; i < s1.size(); ++i)
+        if (tolower(s1[i]) != tolower(s2[i]))
+            return false;
+    return true;
+}
+
+int main(void)
 {
     Phonebook   phonebook;
     std::string command;
@@ -24,17 +35,17 @@ int main()
     {
         std::cout << "Please insert one of the following commands: ADD, SEARCH, EXIT" << std::endl;
         std::getline(std::cin, command);
-        if (command == "EXIT")
+        if (iequals(command, "EXIT"))
         {
             std::cout << "Thank you for using Phonebook." << std::endl;
             break;
         }
-        if (command == "ADD")
+        else if (iequals(command, "ADD"))
         {
             phonebook.add_contact();
             std::cout << "Contact successfully added." << std::endl;
         }
-        else if (command == "SEARCH")
+        else if (iequals(command, "SEARCH"))
         {
             phonebook.print_contacts();
             std::cout << "Please provide the index of the contact you want to see more details" << std::endl;
@@ -49,5 +60,5 @@ int main()
         else
             std::cout << "Invalid command" << std::endl; 
     }
-    return (0);
+    return 0;
 }
